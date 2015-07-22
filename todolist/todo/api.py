b8f7c93fb5a09e0e2eafa508ab2fd504d819f312
Mikhail Mylitsa <atreidas1@gmail.com>
2015-07-22 10:14:22 +0300

    Added Authorzation, Registration,Logining.

--- todolist/todo/api.py
+++ todolist/todo/api.py
@@ -1,23 +1,120 @@
 from django.contrib.auth.models import User
 from tastypie import fields
-from tastypie.resources import ModelResource
+from tastypie.resources import ModelResource, Resource, ALL, ALL_WITH_RELATIONS
 from todo.models import Task
 from tastypie.authentication import SessionAuthentication
+from tastypie.authorization import DjangoAuthorization,Authorization
+from django.contrib.auth import authenticate, login, logout
+from tastypie.http import HttpUnauthorized, HttpForbidden
+from django.conf.urls import  url
+from tastypie.utils import trailing_slash
+from django.core.exceptions import ObjectDoesNotExist
+from authorization import UsersAuthorization, TasksAuthorization
 
+
+###################User resource####################
 
 class UserResource(ModelResource):
     class Meta:
         queryset = User.objects.all()
-        resource_name = 'users'
+        resource_name = 'user'
+        allowed_methods = ['get', 'post']
         authentication = SessionAuthentication()
-        #fields = ['username', 'first_name', 'last_name', 'last_login']
+        authorization=UsersAuthorization()
+        excludes = ['email', 'password', 'is_active', 'is_staff',
+                    'is_superuser','last_login','last_name',
+                    'date_joined','first_name']
         #allowed_methods = ['get']
+        filtering = {
+            'username': ALL,
+        }
 
+###################Task resource####################
 
 class TaskResource(ModelResource):
-    user = fields.ForeignKey(UserResource, 'user')
+  user = fields.ForeignKey(UserResource, 'user')
 
-    class Meta:
-        queryset = Task.objects.all()
-        resource_name = 'tasks'
-        #excludes = ['email', 'password', 'is_active', 'is_staff', 'is_superuser']
+  class Meta:
+    authorization = TasksAuthorization() #Authorization()
+    queryset = Task.objects.all()
+    allowed_methods = ['get', 'post', 'put','patch','delete']
+    resource_name = 'tasks'
+    always_return_data=True
+    filtering = {
+            'user': ALL_WITH_RELATIONS,
+        }
+
+
+###################Login resource####################
+
+class LoginResource(Resource):
+  class Meta:
+    resource_name = 'login'
+
+  def dispatch(self, request_type, request, **kwargs):
+    self.method_check(request, allowed=['post'])
+    data = self.deserialize(request, request.body,
+                            format=request.META.get('CONTENT_TYPE', 'application/json'))
+    username = data.get('username', '')
+    password = data.get('password', '')
+    user = authenticate(username=username, password=password)
+    if user is not None:
+      if user.is_active:
+        login(request,user)
+        return self.create_response(request, {
+                    'success': True
+                    })
+      else:
+        return self.create_response(request, {
+                    'success': False,
+                    'reason': 'disabled',
+                    }, HttpForbidden )
+    else:
+      return self.create_response(request, {
+                  'success': False,
+                  'reason': 'incorrect',
+                  }, HttpUnauthorized )
+
+
+###################Logout resource####################
+class LogoutResource(Resource):
+  class Meta:
+    allowed_methods = ['get']
+    resource_name = 'logout'
+
+  def dispatch(self, request_type, request, **kwargs):
+    if request.user and request.user.is_authenticated():
+      logout(request)
+      return self.create_response(request, { 'success': True })
+    else:
+      return self.create_response(request, { 'success': False }, HttpUnauthorized)
+
+###################Register resource####################
+
+class RegisterResource(Resource):
+  class Meta:
+    resource_name = 'register'
+
+  def dispatch(self, request_type, request, **kwargs):
+    self.method_check(request, allowed=['post'])
+    data = self.deserialize(request, request.body,
+                            format=request.META.get('CONTENT_TYPE', 'application/json'))
+    username = data.get('username', '')
+    password = data.get('password', '')
+    repeatPassword = data.get('repeatpassword', '')
+    if username is not None:
+      try:
+        user=User.objects.get(username=username)
+        return self.create_response(request, { 'success' : False,
+                                               'reason' : 'userexist' })
+      except ObjectDoesNotExist:
+        if password == repeatPassword:
+          user =  User.objects.create_user(username, username+'@thebeatles.com', password)
+          user.save()
+          return self.create_response(request, { 'success' : True})
+        else:
+          return self.create_response(request, { 'success' : False,
+                                                  'reason' : 'differentpass'})
+    else:
+      return self.create_response(request, { 'success' : False,
+                                             'reason' : 'nousername'})
---
+++ todolist/todo/authorization.py
@@ -0,0 +1,101 @@
+from tastypie.authorization import Authorization
+from tastypie.exceptions import Unauthorized
+
+
+class TasksAuthorization(Authorization):
+
+    def check_user(self,user):
+      if user.is_anonymous():
+        raise Unauthorized()
+      else :
+        return user
+
+    def read_list(self, object_list, bundle):
+      current_user=self.check_user(bundle.request.user)
+      return object_list.filter(user=current_user)
+
+    def read_detail(self, object_list, bundle):
+      current_user=self.check_user(bundle.request.user)
+      return bundle.obj.user == current_user
+
+    def create_list(self, object_list, bundle):
+      current_user=self.check_user(bundle.request.user)
+      return object_list
+
+    def create_detail(self, object_list, bundle):
+      current_user=self.check_user(bundle.request.user)
+      return bundle.obj.user == current_user
+
+    def update_list(self, object_list, bundle):
+      current_user=self.check_user(bundle.request.user)
+      allowed = []
+      for obj in object_list:
+        if obj.user == current_user:
+          allowed.append(obj)
+      return allowed
+
+    def update_detail(self, object_list, bundle):
+      current_user=self.check_user(bundle.request.user)
+      return bundle.obj.user == current_user
+
+    def delete_list(self, object_list, bundle):
+      current_user=self.check_user(bundle.request.user)
+      return bundle.obj.user == current_user
+
+
+    def delete_detail(self, object_list, bundle):
+      current_user=self.check_user(bundle.request.user)
+      return bundle.obj.user == current_user
+
+class UsersAuthorization(Authorization):
+
+  def check_user(self,user):
+    if user.is_anonymous():
+      raise Unauthorized()
+    else :
+      return user
+
+  def read_list(self, object_list, bundle):
+    current_user=self.check_user(bundle.request.user)
+    return object_list
+
+  def read_detail(self, object_list, bundle):
+    current_user=self.check_user(bundle.request.user)
+    return True
+
+  def create_list(self, object_list, bundle):
+    current_user=self.check_user(bundle.request.user)
+    return current_user.is_superuser()
+
+  def create_detail(self, object_list, bundle):
+    current_user=self.check_user(bundle.request.user)
+    if current_user.is_superuser():
+      return True
+    else:
+      return bundle.obj.user == current_user
+
+  def update_detail(self, object_list, bundle):
+      current_user=self.check_user(bundle.request.user)
+      if current_user.is_superuser():
+        return True
+      else:
+        return bundle.obj.user == current_user
+
+  def delete_list(self, object_list, bundle):
+    current_user=self.check_user(bundle.request.user)
+    return current_user.is_superuser()
+
+
+  def delete_detail(self, object_list, bundle):
+    current_user=self.check_user(bundle.request.user)
+    if current_user.is_superuser():
+      return True
+    else:
+      return bundle.obj.user == current_user
+
+  def update_list(self, object_list, bundle):
+    current_user=self.check_user(bundle.request.user)
+    if current_user.is_superuser():
+      return allowed
+    else:
+      return []
