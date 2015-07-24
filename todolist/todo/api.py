26d9000083592bdf3f6d1db68bf6f539142ec1d2
Mikhail Mylitsa <atreidas1@gmail.com>
2015-07-24 16:42:38 +0300

    I am corrected your remarks.

--- todolist/todo/api.py
+++ todolist/todo/api.py
@@ -11,7 +11,6 @@ from tastypie.utils import trailing_slash
 from django.core.exceptions import ObjectDoesNotExist
 from authorization import UsersAuthorization, TasksAuthorization
 
-
 ###################User resource####################
 
 class UserResource(ModelResource):
@@ -24,7 +23,6 @@ class UserResource(ModelResource):
         excludes = ['email', 'password', 'is_active', 'is_staff',
                     'is_superuser','last_login','last_name',
                     'date_joined','first_name']
-        #allowed_methods = ['get']
         filtering = {
             'username': ALL,
         }
@@ -32,89 +30,99 @@ class UserResource(ModelResource):
 ###################Task resource####################
 
 class TaskResource(ModelResource):
-  user = fields.ForeignKey(UserResource, 'user')
+    user = fields.ForeignKey(UserResource, 'user')
 
-  class Meta:
-    authorization = TasksAuthorization() #Authorization()
-    queryset = Task.objects.all()
-    allowed_methods = ['get', 'post', 'put','patch','delete']
-    resource_name = 'tasks'
-    always_return_data=True
-    filtering = {
-            'user': ALL_WITH_RELATIONS,
-        }
-
-
-###################Login resource####################
-
-class LoginResource(Resource):
-  class Meta:
-    resource_name = 'login'
-
-  def dispatch(self, request_type, request, **kwargs):
-    self.method_check(request, allowed=['post'])
-    data = self.deserialize(request, request.body,
-                            format=request.META.get('CONTENT_TYPE', 'application/json'))
-    username = data.get('username', '')
-    password = data.get('password', '')
-    user = authenticate(username=username, password=password)
-    if user is not None:
-      if user.is_active:
-        login(request,user)
-        return self.create_response(request, {
-                    'success': True
-                    })
-      else:
-        return self.create_response(request, {
-                    'success': False,
-                    'reason': 'disabled',
-                    }, HttpForbidden )
-    else:
-      return self.create_response(request, {
-                  'success': False,
-                  'reason': 'incorrect',
-                  }, HttpUnauthorized )
-
-
-###################Logout resource####################
-class LogoutResource(Resource):
-  class Meta:
-    allowed_methods = ['get']
-    resource_name = 'logout'
-
-  def dispatch(self, request_type, request, **kwargs):
-    if request.user and request.user.is_authenticated():
-      logout(request)
-      return self.create_response(request, { 'success': True })
-    else:
-      return self.create_response(request, { 'success': False }, HttpUnauthorized)
+    class Meta:
+        authorization = TasksAuthorization()
+        queryset = Task.objects.all()
+        allowed_methods = ['get', 'post', 'put','patch','delete']
+        resource_name = 'tasks'
+        always_return_data=True
+        filtering = {'user': ALL_WITH_RELATIONS,}
 
 ###################Register resource####################
 
 class RegisterResource(Resource):
-  class Meta:
-    resource_name = 'register'
+    class Meta:
+        resource_name = 'register'
 
-  def dispatch(self, request_type, request, **kwargs):
-    self.method_check(request, allowed=['post'])
-    data = self.deserialize(request, request.body,
-                            format=request.META.get('CONTENT_TYPE', 'application/json'))
-    username = data.get('username', '')
-    password = data.get('password', '')
-    repeatPassword = data.get('repeatpassword', '')
-    if username is not None:
-      try:
-        user=User.objects.get(username=username)
-        return self.create_response(request, { 'success' : False,
-                                               'reason' : 'userexist' })
-      except ObjectDoesNotExist:
-        if password == repeatPassword:
-          user =  User.objects.create_user(username, username+'@thebeatles.com', password)
-          user.save()
-          return self.create_response(request, { 'success' : True})
+    def dispatch(self, request_type, request, **kwargs):
+        self.method_check(request, allowed=['post'])
+        data = self.deserialize(request, request.body,
+        format=request.META.get('CONTENT_TYPE', 'application/json'))
+        username = data.get('username', '')
+        password = data.get('password', '')
+        repeatPassword = data.get('repeatpassword', '')
+        if username is not None:
+            try:
+                user=User.objects.get(username=username)
+                return self.create_response(request, { 'success' : False, 'reason' : 'userexist' })
+            except ObjectDoesNotExist:
+                if password == repeatPassword:
+                    user =  User.objects.create_user(username, username+'@thebeatles.com', password)
+                    user.save()
+                    user = authenticate(username=username, password=password)
+                    login(request,user)
+                    return self.create_response(request, { 'success' : True, 'user' : user, 'userId' : user.id})
+                else:
+                    return self.create_response(request, { 'success' : False,'reason' : 'differentpass'})
         else:
-          return self.create_response(request, { 'success' : False,
-                                                  'reason' : 'differentpass'})
-    else:
-      return self.create_response(request, { 'success' : False,
-                                             'reason' : 'nousername'})
+            return self.create_response(request, { 'success' : False,'reason' : 'nousername'})
+
+###################Login and Logout resource####################
+
+class AuthResource(Resource):
+    class Meta:
+        resource_name = 'auth'
+
+    def prepend_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/login%s$" %
+                (self._meta.resource_name, trailing_slash()),
+                self.wrap_view('login'), name="api_login"),
+            url(r'^(?P<resource_name>%s)/logout%s$' %
+                (self._meta.resource_name, trailing_slash()),
+                self.wrap_view('logout'), name='api_logout'),
+        ]
+
+    def login(self, request, **kwargs):
+        self.method_check(request, allowed=['post'])
+        current_user = request.user
+        if current_user.is_anonymous():
+            data = self.deserialize(request, request.body,
+            format=request.META.get('CONTENT_TYPE', 'application/json'))
+            username = data.get('username')
+            password = data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request,user)
+                    return self.create_response(request, {
+                    'success': True,
+                    'user' : user,
+                    'userId' : user.id,
+                    })
+                else:
+                    return self.create_response(request, {
+                    'success': False,
+                    'reason': 'disabled',
+                    }, HttpForbidden )
+            else:
+                return self.create_response(request, {
+                'success': False,
+                'reason': 'incorrect',
+                }, HttpUnauthorized )
+        else:
+            return self.create_response(request, {
+            'success': True,
+            'user': current_user,
+            'userId': current_user.id,
+            })
+
+    def logout(self, request, **kwargs):
+        self.method_check(request, allowed=['get'])
+        if request.user and request.user.is_authenticated():
+            logout(request)
+            return self.create_response(request, { 'success': True })
+        else:
+            return self.create_response(request, { 'success': False }, HttpUnauthorized)
--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from tastypie.api import Api
-from todo.api import TaskResource, UserResource, LoginResource, LogoutResource, RegisterResource
+from todo.api import TaskResource, UserResource, AuthResource, RegisterResource
 
 v1_api = Api(api_name='v1')
 v1_api.register(UserResource())
 v1_api.register(TaskResource())
-v1_api.register(LoginResource())
-v1_api.register(LogoutResource())
+v1_api.register(AuthResource())
 v1_api.register(RegisterResource())
 
 urlpatterns = patterns('',
